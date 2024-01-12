7f3c4e96f90f874dc88caae4e4086e13176bbb49
fand <fandong09@wedoctor.com>
2024-01-12 17:59:51 +0800

    update

---
+++ ios/ReactNativeDemo/TabBarC.swift
@@ -0,0 +1,29 @@
+//
+//  TabBarC.swift
+//  ReactNativeDemo
+//
+//  Created by ybdjk on 2024/1/12.
+//
+
+import UIKit
+
+class TabBarC: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
