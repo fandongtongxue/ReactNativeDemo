c86b1b9bb4a9c6da9528dbc50a7fb45fa441b066
fand <fandong09@wedoctor.com>
2024-01-15 14:47:39 +0800

    导航栏

--- src/contact/index.js
+++ src/contact/index.js
@@ -1,36 +1,58 @@
-import React, {useState} from 'react';
-import {AppRegistry, StyleSheet, View, ActivityIndicator, Button, Alert, Image, Switch, Text, TextInput, SafeAreaView, ScrollView} from 'react-native';
+import React, { useState } from 'react';
+import { AppRegistry, StyleSheet, View, ActivityIndicator, Button, Alert, Image, Switch, Text, TextInput, SafeAreaView, ScrollView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 class UIDemo extends React.Component {
 
   render() {
     return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Details" component={DetailsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
 
-        <ScrollView>
-          <View style={[styles.container, styles.vertical]}>
+const Stack = createNativeStackNavigator();
+
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+function HomeScreen() {
+  return (
+    <ScrollView>
+      <View style={[styles.container, styles.vertical]}>
         <ActivityIndicator />
         <ActivityIndicator size="large" />
         <ActivityIndicator color="red" />
         <ActivityIndicator size="large" color="red" />
 
         <Button
-        title="Press me"
-        color="red"
-        onPress={() => Alert.alert('Simple Button pressed')}/>
+          title="Press me"
+          color="red"
+          onPress={() => Alert.alert('Simple Button pressed')} />
 
-      <Image
-        style={styles.tinyLogo}
-        source={{
-          uri: 'https://reactnative.dev/img/tiny_logo.png',
-        }}
-      />
-      <Switch/>
-      <Text>Hello, World!</Text>
-      <TextInput style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}></TextInput>
+        <Image
+          style={styles.tinyLogo}
+          source={{
+            uri: 'https://reactnative.dev/img/tiny_logo.png',
+          }}
+        />
+        <Switch />
+        <Text>Hello, World!</Text>
+        <TextInput style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}></TextInput>
       </View>
-        </ScrollView>
-    );
-  }
+    </ScrollView>
+  )
 }
 
 const styles = StyleSheet.create({
